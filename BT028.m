a55c34762ced58cb7ead057bfc4c9444ab64cc75
Nguyễn Thanh Phương <32267447+NguyenThanhPhuongdaisy@users.noreply.github.com>
2017-11-06 19:44:37 +0700

    Update BT028.m

--- BT028.m
+++ BT028.m
@@ -4,27 +4,27 @@ function BT028()
     nBins=144; % note
     
     nTrainImages=size(imgTrainAll,2);
-    imgTrainAll_hist=zeros(nBins,nTrainImages);
+    imgTrainAll_hog=zeros(nBins,nTrainImages);
     for i=1:nTrainImages
         imgI=imgTrainAll(:,i);
         img2D=reshape(imgI,28,28);
         [featureVector, hogVisualization]=extractHOGFeatures(img2D);
-        imgTrainAll_hist(:,i)=featureVector;
+        imgTrainAll_hog(:,i)=featureVector;
     end
         
-    Mdl=fitcecoc(imgTrainAll',lblTrainAll);
+    Mdl=fitcecoc(imgTrainAll_hog',lblTrainAll);
     
     imgTestAll=loadMNISTImages('t10k-images.idx3-ubyte');
     lblTestAll=loadMNISTLabels('t10k-labels.idx1-ubyte');
     nTestImages=size(imgTestAll,2);
-    imgTestAll_hist=zeros(nBins,nTestImages);
+    imgTestAll_hog=zeros(nBins,nTestImages);
     for i=1:nTestImages
         imgI=imgTestAll(:,i);
         img2D=reshape(imgI,28,28);
-        imgTestAll_hist(:,i)=extractHOGFeatures(img2D);
+        imgTestAll_hog(:,i)=extractHOGFeatures(img2D);
     end
         
-    lblResult=predict(Mdl,imgTestAll_hist');
+    lblResult=predict(Mdl,imgTestAll_hog');
     nResult=(lblResult==lblTestAll);
     nCount=sum(nResult);
     fprintf('\n So luong mau dung: %d\',nCount);
